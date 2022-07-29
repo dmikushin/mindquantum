7cc0b7a00cf3c211bd0dfc802f2a4607f50bbc93
Dmitry Mikushin <dmitry@kernelgen.org>
2022-07-29 12:37:10 +0200

    Working on the test stub

--- third_party/cppsim/src/test/test_nointrin.cpp
+++ third_party/cppsim/src/test/test_nointrin.cpp
@@ -10,8 +10,56 @@ namespace generated {
 
 } // namespace generated
 
+#include <array>
+
+#include "gtest/gtest.h"
+
+template<int nqubits>
+bool compare()
+{
+	constexpr auto dim = 1UL << nqubits;
+	
+	// TODO Generate m matrix as integers.
+	std::array<std::array<int, nqubits>, nqubits> m;
+	
+	// TODO Replace std::complex with auto.
+	
+	// TODO Generate id_0...id_nq integers (unsorted).
+	
+	// TODO Generate psi matrix as integers.
+	
+	// TODO Compare kernel against generated kernel
+	return true;
+}
+
+TEST(nointrin, kernel1)
+{
+	ASSERT_TRUE(compare<1>);
+}
+
+TEST(nointrin, kernel2)
+{
+	ASSERT_TRUE(compare<2>);
+}
+
+TEST(nointrin, kernel3)
+{
+	ASSERT_TRUE(compare<3>);
+}
+
+TEST(nointrin, kernel4)
+{
+	ASSERT_TRUE(compare<4>);
+}
+
+TEST(nointrin, kernel5)
+{
+	ASSERT_TRUE(compare<5>);
+}
+
 int main(int argc, char* argv[])
 {
-	// TODO GoogleTest
-	return 0;
+	::testing::InitGoogleTest( & argc, argv);
+	return RUN_ALL_TESTS();
 }
+
