1331e413066e103313e9b76aecf3b819a34b6781
Dmitry Mikushin <dmitry@kernelgen.org>
2022-08-23 19:10:38 +0200

    Finalizing better representation of the generator script

--- third_party/cppsim/include/nointrin/kernelgen.py
+++ third_party/cppsim/include/nointrin/kernelgen.py
@@ -39,9 +39,6 @@ def kernelgen(nqubits, ids=None):
 
     # Some string constants clash with the {} syntax of print(), so we
     # substitute them as constants.
-    define = "#define"
-    undef = "#undef"
-    pragma = "#pragma"
     newline = "\n"
 
     kernel = \
@@ -59,7 +56,7 @@ def kernelgen(nqubits, ids=None):
 template<{d_template}class T>
 inline void kernel_core(T* psi, std::size_t I{d_var}, const T* m)
 {{
-    std::array v = {{{v_array}}};
+    std::array {v_array};
 
     {psi_assign}
 }}
@@ -72,41 +69,59 @@ inline void kernel_core(T* psi, std::size_t I{d_var}, const T* m)
         nqubits    = nqubits, \
         d_template = ''.join('std::size_t d{}, '.format(i) for i in range (0, nqubits)) if ids != None else '', \
         d_var      = ''.join(', std::size_t d{}'.format(i) for i in range (0, nqubits)) if ids == None else '', \
-        v_array    = newline + ''.join('        {},{}'.format(strcombs[i], newline) for i in range(0, len(strcombs))) + '    ', \
+        v_array    = f"v = {{{newline}" + ''.join('{}{},{}'.format(' ' * 8, strcombs[i], newline) for i in range(0, len(strcombs))) + "{}}}".format(' ' * 4), \
         psi_assign = ''.join('{} = {};{}    '.format(strcombs[i], strrhs[i], newline) for i in range(0, len(strcombs)))) + \
-f"""
-{define} LOOP_COLLAPSE{nqubits} ({nqubits} + 1) 
-
+"""
 // bit indices id[.] are given from high to low (e.g. control first for CNOT)
 template<class T>
-void kernel(T* psi, {''.join('unsigned id{}, '.format(nqubits - i - 1) for i in range (0, nqubits)) if ids == None else ''}const T* m, std::size_t ctrlmask)
+void kernel(T* psi, {id_var}const T* m, std::size_t ctrlmask)
 {{
-    {'constexpr ' if ids != None else ''}std::size_t d0 = 1UL << {'id0' if ids == None else ids[0]}{''.join(', d{} = 1UL << {}'.format(i, 'id{}'.format(i) if ids == None else ids[i]) for i in range (1, nqubits))};
-    {'constexpr ' if ids != None else ''}std::size_t n = 1{''.join(' + d{}'.format(i) for i in range (0, nqubits))};
-    {'constexpr ' if ids != None else ''}std::size_t dsorted[] = {{ d{nqubits - 1}{''.join(', d{}'.format(nqubits - i - 1) for i in range (1, nqubits))} }};
-    {'std::sort(dsorted, dsorted + {}, std::greater<std::size_t>());{}'.format(nqubits, newline) if ids == None else ''}
+    {constexpr}std::size_t {d};
+    {constexpr}std::size_t {n};
+    {constexpr}std::size_t {dsorted};
+    {sort}
     if (ctrlmask == 0){{
-        {pragma} omp for collapse(LOOP_COLLAPSE{nqubits}) schedule(static)
+        {pragma} omp for collapse({collapse}) schedule(static)
         for (std::size_t i0 = 0; i0 < n; i0 += 2 * dsorted[0]){{
-{''.join('{}for (std::size_t i{} = 0; i{} < dsorted[{}]; i{} += 2 * dsorted[{}]){}'.format(''.join('    ' for j in range(0, i + 2)), i, i, i - 1, i, i, newline) for i in range (1, nqubits))}{''.join('    ' for i in range (0, nqubits + 2))}for (std::size_t i{nqubits} = 0; i{nqubits} < dsorted[{nqubits - 1}]; ++i{nqubits}){{
-        {''.join('    '.format(i) for i in range (1, nqubits + 2))}kernel_core{'<d0' if ids != None else ''}{''.join(', d{}'.format(i) for i in range (1, nqubits)) if ids != None else ''}{'>' if ids != None else ''}(psi, i0{''.join(' + i{}'.format(i) for i in range (1, nqubits + 1))}, {''.join('d{}, '.format(i) for i in range (0, nqubits)) if ids == None else ''}m);
-        {''.join('    '.format(i) for i in range (1, nqubits + 1))}}}
+{for_loops}{offset_2}for (std::size_t i{nqubits} = 0; i{nqubits} < dsorted[{nqubits_1}]; ++i{nqubits}){{
+        {offset_1}kernel_core{d_template}(psi, {i}, {d_args}m);
+        {offset}}}
         }}
     }}
     else{{
-        {pragma} omp for collapse(LOOP_COLLAPSE{nqubits}) schedule(static)
+        {pragma} omp for collapse({collapse}) schedule(static)
         for (std::size_t i0 = 0; i0 < n; i0 += 2 * dsorted[0]){{
-{''.join('{}for (std::size_t i{} = 0; i{} < dsorted[{}]; i{} += 2 * dsorted[{}]){}'.format(''.join('    ' for j in range(0, i + 2)), i, i, i - 1, i, i, newline) for i in range (1, nqubits))}{''.join('    ' for i in range (0, nqubits + 2))}for (std::size_t i{nqubits} = 0; i{nqubits} < dsorted[{nqubits - 1}]; ++i{nqubits}){{
-        {''.join('    '.format(i) for i in range (1, nqubits + 2))}if (((i0{''.join(' + i{}'.format(i) for i in range (1, nqubits + 1))})&ctrlmask) == ctrlmask)
-        {''.join('    '.format(i) for i in range (1, nqubits + 3))}kernel_core{'<d0' if ids != None else ''}{''.join(', d{}'.format(i) for i in range (1, nqubits)) if ids != None else ''}{'>' if ids != None else ''}(psi, i0{''.join(' + i{}'.format(i) for i in range (1, nqubits + 1))}, {''.join('d{}, '.format(i) for i in range (0, nqubits)) if ids == None else ''}m);
-        {''.join('    '.format(i) for i in range (1, nqubits + 1))}}}
+{for_loops}{offset_2}for (std::size_t i{nqubits} = 0; i{nqubits} < dsorted[{nqubits_1}]; ++i{nqubits}){{
+        {offset_1}if ((({i})&ctrlmask) == ctrlmask)
+        {offset_2}kernel_core{d_template}(psi, {i}, {d_args}m);
+        {offset}}}
         }}
     }}
 }}
 
-{undef} LOOP_COLLAPSE{nqubits}
+{undef} add
+{undef} mul
 
-"""
+""".format( \
+        define      = "#define", \
+        undef       = "#undef", \
+        pragma      = "#pragma", \
+        nqubits     = nqubits, \
+        nqubits_1   = nqubits - 1, \
+        id_var      = ''.join('unsigned id{}, '.format(nqubits - i - 1) for i in range (0, nqubits)) if ids == None else '', \
+        constexpr   = 'constexpr ' if ids != None else '',
+        d           = f"d0 = 1UL << {'id0' if ids == None else ids[0]}{''.join(', d{} = 1UL << {}'.format(i, 'id{}'.format(i) if ids == None else ids[i]) for i in range (1, nqubits))}", \
+        d_args      = ''.join('d{}, '.format(i) for i in range (0, nqubits)) if ids == None else '', \
+        n           = 'n = 1' + ''.join(' + d{}'.format(i) for i in range (0, nqubits)), \
+        dsorted     = f"dsorted[] = {{ d{nqubits - 1}" + ''.join(', d{}'.format(nqubits - i - 1) for i in range (1, nqubits)) + f" }}", \
+        sort        = f'std::sort(dsorted, dsorted + {nqubits}, std::greater<std::size_t>());{newline}' if ids == None else '', \
+        collapse    = f"{nqubits + 1}", \
+        offset    = ''.join('    '.format(i) for i in range (0, nqubits)), \
+        offset_1    = ''.join('    '.format(i) for i in range (0, nqubits + 1)), \
+        offset_2    = ''.join('    '.format(i) for i in range (0, nqubits + 2)), \
+        d_template  = ('<d0' + ''.join(', d{}'.format(i) for i in range (1, nqubits)) + '>') if ids != None else '', \
+        i           = 'i0' + ''.join(' + i{}'.format(i) for i in range (1, nqubits + 1)), \
+        for_loops   = ''.join('{}for (std::size_t i{} = 0; i{} < dsorted[{}]; i{} += 2 * dsorted[{}]){}'.format(''.join('    ' for j in range(0, i + 2)), i, i, i - 1, i, i, newline) for i in range (1, nqubits)))
 
     return kernel
 
