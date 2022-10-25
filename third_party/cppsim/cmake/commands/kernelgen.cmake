f4fdf202aa2c38d4e0d609ea3e11fdfdebfc00c7
Damien Nguyen <ngn.damien@gmail.com>
2022-10-25 14:52:37 +0200

    Improve CXX standard handling for all targets + add .cmake-format.yaml file

--- third_party/cppsim/cmake/commands/kernelgen.cmake
+++ third_party/cppsim/cmake/commands/kernelgen.cmake
@@ -18,13 +18,12 @@ function(kernelgen)
     set(CPPSIM_INCLUDE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/include)
   endif()
   set(KERNELGEN "${CPPSIM_INCLUDE_DIR}/${VARIANT}/kernelgen.py")
-  set(KERNEL_PATH
-      "${CMAKE_CURRENT_BINARY_DIR}/generated/${VARIANT}/kernel${NQUBITS}.hpp")
-      
+  set(KERNEL_PATH "${CMAKE_CURRENT_BINARY_DIR}/generated/${VARIANT}/kernel${NQUBITS}.hpp")
+
   if(NOT EXISTS "${KERNELGEN_PY}")
     message(FATAL_ERROR "Cannot locate kernelgen Python script: ${KERNELGEN_PY}")
   endif()
-  
+
   set(_args)
   if(KERNELGEN_COMBINATIONS)
     list(APPEND _args --combinations=True)
@@ -33,14 +32,13 @@ function(kernelgen)
   # Call generator.
   add_custom_command(
     OUTPUT ${KERNEL_PATH}
-    COMMAND ${Python_EXECUTABLE} ${KERNELGEN_PY} ${NQUBITS} ${KERNEL_PATH}
-            ${_args}
+    COMMAND ${Python_EXECUTABLE} ${KERNELGEN_PY} ${NQUBITS} ${KERNEL_PATH} ${_args}
     COMMENT "Generating kernel for ${NQUBITS} qubits"
     DEPENDS ${KERNELGEN_PY})
   set_source_files_properties("${KERNEL_PATH}" PROPERTIES GENERATED TRUE)
 
   # Append the generated file to the target sources.
   target_sources(${KERNELGEN_TARGET} PRIVATE ${KERNEL_PATH})
-  target_include_directories(${KERNELGEN_TARGET}
-                             PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+  target_include_directories(${KERNELGEN_TARGET} PRIVATE ${CMAKE_CURRENT_BINARY_DIR})
+  target_compile_features(${KERNELGEN_TARGET} PRIVATE cxx_std_17)
 endfunction()
